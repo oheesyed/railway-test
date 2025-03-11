04f93b0809f0661f602e7f1e3d48a7ca6c52297a
Ohee Syed <136873445+oheesyed@users.noreply.github.com>
2025-03-11 07:56:17 -0400

    local gradio added

--- src/personal_assistant_agency/agency.py
+++ src/personal_assistant_agency/agency.py
@@ -1,3 +1,4 @@
+import sys
 from agency_swarm import Agency
 from .PersonalAssistant.PersonalAssistant import PersonalAssistant
 
@@ -9,15 +10,13 @@ agency = Agency(
     [assistant],  # Single agent setup with PersonalAssistant as the entry point
     shared_instructions="agency_manifesto.md",
     temperature=0.5,
-    max_prompt_tokens=32000  # Increased from 25000 to 32000
+    max_prompt_tokens=32000,  # Increased from 25000 to 32000
 )
 
 if __name__ == "__main__":
     # Check command line arguments
     if len(sys.argv) > 1 and sys.argv[1] == "--gradio":
-        agency.demo_gradio(
-            share=True  # Set to True to get a public URL
-        )
+        agency.demo_gradio(share=True)  # Set to True to get a public URL
     else:
         # Default to demo mode
-        agency.run_demo() 
+        agency.run_demo()
